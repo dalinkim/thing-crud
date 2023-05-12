ee62aa12ddc4645953b80dd487f8a92857345b39
dalin <44915457+dalinkim@users.noreply.github.com>
2023-05-11 22:19:41 -0500

    init spring boot

---
+++ src/main/java/com/github/dalink/thing/ThingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.github.dalink.thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingCrudApplication.class, args);
+	}
+
+}
